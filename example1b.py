bf4d3b07565139e0463ec21bd1da6696a65578ca
hungrybeagle <cyang@deltalearns.ca>
2024-12-12 09:29:00 -0800

    Update example1b.py

--- example1b.py
+++ example1b.py
@@ -10,10 +10,10 @@ but can also be a negative number, indicating position from the end of the tuple
 myTuple = ('Alpha', 'Baker', "Charlie", "Delta", "Echo","Foxtrot","Golf")
 print("\n======")
 print(myTuple)
-print(" tuple[0] is " + myTuple[0])
-print(" tuple[1] is " + myTuple[1])
-print(" tuple[-1] is " + myTuple[-1])
-print(" tuple[-3] is " + myTuple[-3])
+print(f" tuple[0] is {myTuple[0]}")
+print(f" tuple[1] is {myTuple[1]}")
+print(f" tuple[-1] is {myTuple[-1]}")
+print(f" tuple[-3] is {myTuple[-3]}")
 '''
 A range of values from a tuple can be printed using the : to indicate the
 beginning and ending.
@@ -22,12 +22,12 @@ to the beginning or to the end
 '''
 print("\n== For printing a range, note which indexes are included and which are not...")
 print("== the 1st index is inclusive and the index after the : is not included")
-print(" tuple[1:3] is " + str(myTuple[1:3]))
+print(f" tuple[1:3] is {myTuple[1:3]}")
 print(" tuple[:2] is ", end="")
 print(myTuple[:2])
 print(" tuple[2:] is ", end="")
 print(myTuple[2:])
-print(" tuple[-4:-1] is " + str(myTuple[-4:-1]))
+print(f" tuple[-4:-1] is {myTuple[-4:-1]}")
 
 
 
